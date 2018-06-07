9a356de1ed125d756f3051dd64996900dc4aae71
gleb-britecore <gsev@diceus.com>
2018-06-07 20:11:12 +0300

    db connstr

--- djangok8/settings/production.py
+++ djangok8/settings/production.py
@@ -10,3 +10,12 @@ from .common import *
 # }
 
 LOGGING_ENVIRONMENT = 'production'
+
+# postgres://USER:PASSWORD@HOST:PORT/NAME
+DATABASES = {
+    'default': dj_database_url.config(
+      env='ENV_DATABASE_URL',
+      default='postgres://postgres:5HE1Br0eRb@db-postgresql.gitlab-4.svc.cluster.local/postgres'
+    ),
+}
+
